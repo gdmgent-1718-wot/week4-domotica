ab7c716c2bb82bf238598615396307af4e56abfe
PietVlem <pieter.vleminckx@student.arteveldehs.be>
2017-10-17 15:33:44 +0200

    test

---
+++ src/pages/home/home.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  items: any[];
+
+
+  constructor(public navCtrl: NavController) {
+    this.items = [];
+    for (let i = 0; i < 10; i++){
+      this.items.push({
+        text: "item",
+        id: i
+      })
+    }
+  }
+
+}
